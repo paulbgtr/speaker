43b35a017fcfb417d4d43e44a99af3ae749ccf02
PavelB <pavel@paulbg.dev>
2026-06-17 16:16:00 +0300

    add config file

---
+++ src/config.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define I2S_BCLK 5
+#define I2S_LRC 4
+#define I2S_DIN 6
+
+#define OLED_SDA 1
+#define OLED_SCL 2
+
+#define TIMEZONE 3
